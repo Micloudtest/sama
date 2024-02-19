8d73b5c0a216031e12f75ec0268f7454af56395f
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2024-02-20 06:06:19 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,45 +1,3 @@
-#!/usr/bin/env bash
-#
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
-#
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
-#
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-#
-# 	Please maintain this if you use this script or any part of it
-#
-
-FDEVICE="gale"
-#set -o xtrace
-
-fox_get_target_device() {
- local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
- if [ -n "$chkdev" ]; then
-  FOX_BUILD_DEVICE="$FDEVICE"
- else
-  chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
-  [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
- fi
-}
-
-if [ -z "$1" ] && [ -z "$FOX_BUILD_DEVICE" ]; then
-  fox_get_target_device
-fi
-
-# Dirty Fix: Only declare orangefox vars when needed
-if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
-  echo -e "\x1b[96m[INFO]: Setting up OrangeFox build vars for gale...\x1b[m"
- if [ "$1" = "$FDEVICE" ] || [  "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 # Version / Maintainer infos
 export OF_MAINTAINER="Tapin Recovery Instraller"
 export FOX_VERSION=R12.1_0
@@ -113,5 +71,3 @@ export FOX_REPLACE_TOOLBOX_GETPROP=1
    export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
    export | grep "TW_" >> $FOX_BUILD_LOG_FILE
   fi
- fi
-fi
